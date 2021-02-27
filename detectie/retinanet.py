2144a028bd8d88f7e452ecf2a0cf2b614f462f53
Emile Carron <emile.carron@student.kuleuven.be>
2021-02-27 11:23:28 +0100

    Add dataloaders to retinanet.py

--- detectie/retinanet.py
+++ detectie/retinanet.py
@@ -9,8 +9,16 @@ from torch.optim import SGD
 import sku
 from sku import Sku
 
-model = torchvision.models.detection.retinanet_resnet50_fpn(pretrained=True)
-model.eval()
-x = [torch.rand(3, 300,400), torch.rand(3,500,400)]
-predictions = models(x)
 
+if __name__ == '__main':
+    
+    train_set = Sku(csv_file = '/Users/emilecarron/Documents/School/Universiteit/1ma/Masterproef/Tutorial/5/SKU110K_fixed/annotations/annotations_train.csv',root_dir = '/Users/emilecarron/Documents/School/Universiteit/1ma/Masterproef/Tutorial/5/SKU110K_fixed/images')
+    val_set = Sku(csv_file = '/Users/emilecarron/Documents/School/Universiteit/1ma/Masterproef//Tutorial/5/SKU110K_fixed/annotations/annotations_val.csv',root_dir = '/Users/emilecarron/Documents/School/Universiteit/1ma/Masterproef/Tutorial/5/SKU110K_fixed/images')
+    
+    train= DataLoader(train_set, batch_size=12, num_workers=4)
+    val = DataLoader(val_set, batch_size=12, num_workers=4)
+    
+    model = torchvision.models.detection.retinanet_resnet50_fpn(pretrained=False, progress=True, num_classes=1, pretrained_backbone=True, **kwargs
+    model.eval()
+    trainer = pl.Trainer(gpus=1 if torch.cuda.is_available() else 0, max_epochs=10,)
+    trainer.fit(model, train, val)
