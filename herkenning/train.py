f11dea4cc3813dbbc296f73f36236a0b123a2e2c
nickdehaene <nick.dehaene@hotmail.com>
2021-03-15 16:07:22 +0100

    changes

--- herkenning/train.py
+++ herkenning/train.py
@@ -10,6 +10,7 @@ from torch.optim import SGD
 import prod10k
 from prod10k import Prod10k
 import sys
+from herkenning import HerkenningsModel
 
 def main(arguments):
 
@@ -21,7 +22,7 @@ def main(arguments):
     train= DataLoader(train_set, batch_size=1, num_workers=int(arguments[1]))
     val = DataLoader(val_set, batch_size=1, num_workers=int(arguments[1]))
     
-    model = RetinaNetLightning()
+    model = HerkenningsModel()
     
     trainer = pl.Trainer(gpus=1 if torch.cuda.is_available() else 0, max_epochs=int(arguments[2]))
     trainer.fit(model, train, val)
