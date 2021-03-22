c3e3569259cf13cc7740f377759349fc8b20fc04
Emile Carron <emile.carron@student.kuleuven.be>
2021-03-22 16:32:23 +0100

    Modified DataModule

--- herkenning/recognition/train.py
+++ herkenning/recognition/train.py
@@ -7,11 +7,20 @@ import torchvision
 from torchvision import transforms
 from torch.utils.data import DataLoader
 from torch.optim import SGD
-import Prod10k
-from Prod10k import Prod10k
-from retinanet import RetinaNetLightning
 import sys
 from argparse import ArgumentParser
+from recognition import RecognitionModel
+import json
+from torchvision.datasets.utils import (download_and_extract_archive,
+                                        download_url)
+from aliproducts import AliProducts
+from PIL import Image
+from copy import deepcopy
+
+
+
+BASE_URL = ("https://tianchi-public-us-east-download.oss-us-east-1."
+            "aliyuncs.com/231780/")
 
 SAMPLE_DATA_URL = {
     'data': BASE_URL + "AliProducts_train_sample.tar.gz",
@@ -40,13 +49,15 @@ class Prod10kDataModule(pl.LightningDataModule):
 class AliproductsDataModule(pl.LightningDataModule):
     def __init__(self):
         super().__init__()
-        self.root = args.data_dir/Aliproducts
-        imgs_json = self.root / 'AliProducts_train_sample.json'
+        self.root = args.data_dir + '/Aliproducts'
+        imgs_json = self.root + '/AliProducts_train_sample.json'
+        print(imgs_json)
+        fObj = open(imgs_json,)
         self.img_labels = [
             (img['id'], img['category_id'])
-            for img in json.load(imgs_json.open())['images']
+            for img in json.load(fObj)['images']
         ]
-        self.img_dir = self.root / 'train'
+        self.img_dir = self.root + '/train'
 
     def prepare_data(self):
         download_and_extract_archive(SAMPLE_DATA_URL['data'], self.root)
@@ -54,27 +65,23 @@ class AliproductsDataModule(pl.LightningDataModule):
     
     def setup(self, stage=None):
         if stage == 'fit' or stage is None:
-            self.train_set = Aliproducts(root = self.root)
+            self.train_set = AliProducts(root = self.root, img_labels = self.img_labels)
     
     def train_dataloader(self):
-        return DataLoader(self.train_set, batch_size = self.batch_size, num_workers = args.num_workers)
-        
-    def val_dataloader(self):
-        return DataLoader(self.val_set, batch_size = self.batch_size, num_workers = args.num_workers)
+        return DataLoader(self.train_set, batch_size = args.batch_size, num_workers = args.num_workers)
 
-
-
-def main(arguments):
+def main(arg):
     
     model = RecognitionModel()
     dm = AliproductsDataModule()
     
-    trainer = pl.Trainer(gpus=1 if torch.cuda.is_available() else 0, max_epochs=int(arguments[2]))
+    trainer = pl.Trainer(gpus=1 if torch.cuda.is_available() else 0, max_epochs=args.max_epochs)
     trainer.fit(model, dm)
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--num_workers', type=int, default=12)
+    parser.add_argument('--max_epochs', type=int, default=1)
     parser.add_argument('--weight_decay', type=int, default=0)
     parser.add_argument('--momentum', type=float, default=0.95)
     parser.add_argument('--lr', type=int, default=1e-3)
