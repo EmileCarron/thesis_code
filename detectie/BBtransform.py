692d94e9af29e73388bfbea9ba41159f22c6dc52
Emile Carron <emile.carron@student.kuleuven.be>
2021-04-06 12:51:53 +0200

    clean up code

--- detectie/BBtransform.py
+++ detectie/BBtransform.py
@@ -9,43 +9,14 @@ import cv2
 
 class BBtrans(Dataset):
       
-    # This function prints the type
-    # of the object passed as well
-    # as the object item
-    #def __init__(self):
-    
-    
-#    def __call__(self, target, targettrans):
-#
-#        counter = 0
-#        for x in target['boxes']:
-#            #print(target)
-#            #print(targettrans)
-#            x[0] = targettrans['boxes'][counter][0]
-#            x[1] = targettrans['boxes'][counter][1]
-#            x[2] = targettrans['boxes'][counter][2]
-#            x[3] = targettrans['boxes'][counter][3]
-#            counter = counter + 1
-#
-#        #print(target)
-#        #print(targettrans)
-#
-#        return target
-   
     def __call__(self, target, size):
         size1 = size["size"][0]
         w = size["size"][0][0]
         h = size["size"][0][1]
-        #print(size1)
-#        print(w, h)
-#        print(target)
         counter = 0
         #import pdb; pdb.set_trace()
         b = target['boxes']
         l = target['labels']
-        #y = t[:, np.r_[:1, 3:]]
-        #t = torch.cat((y[:,:3], y[:,4:]))
-        #y = np.delete(t, 2, 0)
         for x in target['boxes']:
             x1 = x[0]
             y1 = x[1]
@@ -73,10 +44,7 @@ class BBtrans(Dataset):
                 counter = counter - 1
                 
             counter = counter + 1
-        #print(target['boxes'])
         target['boxes'] = b
         target['labels'] = l
-        
-        #print(target)
         return target
 
