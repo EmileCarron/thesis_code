c0bde55ce4ed1c1a59e3807fcdd2b41b6c3ab4cc
Emile Carron <emile.carron@student.kuleuven.be>
2021-04-07 15:51:07 +0200

    Change file to fix wrong bbox

--- detectie/BBtransform.py
+++ detectie/BBtransform.py
@@ -17,34 +17,27 @@ class BBtrans(Dataset):
         #import pdb; pdb.set_trace()
         b = target['boxes']
         l = target['labels']
-        for x in target['boxes']:
-            x1 = x[0]
-            y1 = x[1]
-            x2 = x[2]
-            y2 = x[3]
-            
-            if x2 >= w:
-                b = np.delete(b, counter, 0)
-                l = np.delete(l, counter, 0)
-                counter = counter - 1
-
-            if y2 >= h:
-                b = np.delete(b, counter, 0)
-                l = np.delete(l, counter, 0)
-                counter = counter - 1
-                
-            if x1 >= x2:
-                b = np.delete(b, counter, 0)
-                l = np.delete(l, counter, 0)
-                counter = counter - 1
-            
-            if y1 >= y2:
-                b = np.delete(b, counter, 0)
-                l = np.delete(l, counter, 0)
-                counter = counter - 1
-                
-            counter = counter + 1
-        target['boxes'] = b
-        target['labels'] = l
+        colum0 = b[:,0]
+        colum1 = b[:,1]
+        colum2 = b[:,2]
+        colum3 = b[:,3]
+        
+        colum2[colum2 > w] = w
+        colum3[colum3 > h] = h
+        
+        testarray1 = colum2>colum0
+        testarray2 = colum3>colum1
+        
+        newcolum0 = np.where(testarray1, colum0, colum2)
+        newcolum2 = np.where(testarray1, colum2, colum0)
+        
+        newcolum1 = np.where(testarray2, colum1, colum3)
+        newcolum3 = np.where(testarray2, colum3, colum1)
+        
+        b[:,0] = newcolum0
+        b[:,1] = newcolum1
+        b[:,2] = newcolum2
+        b[:,3] = newcolum3
+        
         return target
 
