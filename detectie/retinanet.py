e70950474c4620513c35222c437f29b13290b9ad
Emile Carron <emile.carron@student.kuleuven.be>
2021-04-19 21:22:07 +0200

    change val_set

--- detectie/retinanet.py
+++ detectie/retinanet.py
@@ -21,7 +21,7 @@ class RetinaNetLightning(pl.LightningModule):
         self.save_hyperparameters()
         
     def training_step(self, batch, batch_idx):
-        #import pdb; pdb.set_trace()
+        import pdb; pdb.set_trace()
         x, y = batch
         y = [{'boxes': b, 'labels': l}
         for b, l in zip(y['boxes'],y['labels'])
