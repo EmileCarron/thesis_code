eb0ef9e295ee55e8bef0d54e837291ee65b36841
Emile Carron <emile.carron@student.kuleuven.be>
2021-04-22 16:58:40 +0200

    change val step

--- detectie/retinanet.py
+++ detectie/retinanet.py
@@ -43,6 +43,7 @@ class RetinaNetLightning(pl.LightningModule):
         #print(losses[0]['scores'])
         #loss = torch.argmax(detections[0]['scores'])
         self.log("valid_score", detections[0]['scores'][0], on_step=True, on_epoch=True)
+        return detections
        
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr = self.args.lr,
