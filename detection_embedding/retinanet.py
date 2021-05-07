916e8f0ed33868d3e1bfc1424ac7aa1791e72bc7
Emile Carron <emile.carron@student.kuleuven.be>
2021-05-07 10:41:56 +0200

    add debug code

--- detection_embedding/retinanet.py
+++ detection_embedding/retinanet.py
@@ -229,6 +229,7 @@ class RetinaNetLightning(pl.LightningModule):
             _, predicted = torch.max(predictions.data, 1)
             predictions_embedding = torch.squeeze(predictions_embedding)
             predictions_embedding = predictions_embedding.clone().detach().numpy()
+            predictions_embedding = torch.Tensor(predictions_embedding)
             y[0]['labels'][counter] = predicted 
             
                 #test = torch.no_grad(predictions)
