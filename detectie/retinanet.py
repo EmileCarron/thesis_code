4f35c0cc8c1ab8864f09193b56aa1d044dffecf7
Emile Carron <emile.carron@student.kuleuven.be>
2021-05-14 14:09:17 +0200

    delete debugger

--- detectie/retinanet.py
+++ detectie/retinanet.py
@@ -79,7 +79,7 @@ class RetinaNetLightning(pl.LightningModule):
         ]
 
 
-        import pdb; pdb.set_trace()
+        #import pdb; pdb.set_trace()
         detections = self.model(x,y)
         #print(detections)
         return detections
